21abb01a3b4b40bd50c362fef18a1cbea6a29750
Carlo Ceriotti <carlo.ceriotti@hotmail.it>
2014-12-08 19:49:08 +0100

    OK2

--- cachematrix.R
+++ cachematrix.R
@@ -1,57 +1,61 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-#This function take a square matrix as input, then create a list of functions.
-#At the beginning im (=inverse matrix) is equal to NULL.
+#This function takes a square matrix as input, then create a list of functions (below explained)
+#At the beginning inv_matrix is equal to NULL.
 
-#Set_mat function set m<-NULL and is able to change matrix x with y value.
+#Set_matrix function set inv_matrix<-NULL and if needed change the matrix x with y value.
 
-#Get_mat provides the input matrix
+#Get_matrix provides the original matrix (mat)
 
-#Set_im set the inverse matrix. This is the key step because
-#the << operator (below in HERE line) changes the value of m globally
-#so the NULL value that m had at the beginning is substituted by im.
-#We can say that the operator << (in m<<-im) has the power to change the m value
-#"GLOBALLY", so that the functions "get_mat,get_im,set_mat" now see this m value and
-#no NULL value for m.
+#Set_invmat set the inverse matrix. This is the key step because
+#the << operator (in inv_matrix <- NULL) changes the value of inv_matrix globally
+#so the NULL value that inv_matrix had at the beginning is substituted by inv_matrix.
+#We can say that the operator <<  change the inv_matrix value
+#"GLOBALLY", so that the functions below "get,set..." now can see this inv_matrix value.
+
 
 #Get_im gets the inverse matrix
 
 #TIP to understand better: if you are a little bit familiar with "Object Programming"
-#m is like as attribute and methods are the functions below
+#inv_matrix is the object attribute and objects methods are the 4 functions below
 
-makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL #HERE
-    set_mat <- function(y) {
-      x <<- y
-      m <<- NULL
+makeCacheMatrix <- function(mat = matrix()) {
+    inv_matrix <- NULL #set inverse matrix as NULL
+    
+    set_matrix <- function(y) {
+      mat <<- y
+      inv_matrix <<- NULL
     }
-    get_mat <- function() x
-    set_im <- function(im) m <<- im
-    get_im <- function() m
-    list(set_mat = set_mat, get_mat = get_mat,
-         set_im = set_im,
-         get_im = get_im)
+    
+    get_matrix <- function() mat #get value of original matrix
+    set_invmat <- function(m) inv_matrix <<- m #Set a value for inverse matrix
+    get_invmat <- function() inv_matrix #Retrieve inverse matrix
+    
+    list(set_matrix = set_matrix, get_matrix = get_matrix,
+         set_invmat = set_invmat,
+         get_invmat = get_invmat) #Create a list of functions that will be used by cacheSolve function 
   }
 
 
 ## Write a short comment describing this function
-#This function try to get the inverse matrix. If it has not been calculated yet
-#(and in this case the if block is not executed), the inverse matrix is calculated (im)
-#and im is set as global variable in makeCacheMatrix function (above).
-#If the im is already calculated, the if block is executed and the function exit
-#when it hits "return(im)". In this case im is no calculated but retrieved by 
-#makeCacheMatrix function!
+#This function get the inverse matrix: if it has not been calculated yet
+#(and in this case the if block is not executed), the inverse matrix is calculated
+#and inv_matrix is set using "get_invmat" and this value modify the inv_matrix value in makeCacheMatrix function above.
+
+#If the inv_matrix is already calculated, the if block is executed and the function exits
+#when it hits "return(inv_matrix)". In this case inv_matrix is no calculated again but it is retrieved by 
+#makeCacheMatrix function that stored it in the last execution.
 
 
 cacheSolve <- function(x, ...) {
-    im <- x$get_im()
-    if(!is.null(im)) {
+    inv_matrix <- x$get_invmat() #Try to get inv_matrix value...
+    if(!is.null(inv_matrix)) { #if block executed if inv_matrix is already calculated.
       message("getting cached data")
-      return(im)
+      return(inv_matrix) #Exit the function without any calculation
     }
-    data <- x$get_mat()
-    im <- solve(data, ...)
-    x$set_im(im)
-    im
+    data <- x$get_matrix() #If inv_matrix was NULL...
+    inv_matrix <- solve(data, ...) #Calculate inv_matrix
+    x$set_invmat(inv_matrix) #Set inv_matrix with set_invmat function
+    inv_matrix #Print inverse matrix
   }
